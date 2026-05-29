93536b3e74e090779745867286e6fa973642a1a9
liOnux <lio07@mailo.com>
2026-05-29 22:52:03 +0200

    Corrections bugs mineurs

--- cypress/e2e/createCards.cy.js
+++ cypress/e2e/createCards.cy.js
@@ -6,12 +6,12 @@ describe("Dynamic Card Creation", () => {
 	it("should create skill cards from JSON data", () => {
 	  // Vérifier que les cartes sont bien créées
 	  cy.get("#skills .container .card.skillsText").should("be.visible").and("have.length.greaterThan", 0);
-	  cy.get("#skills .container .card.skillsText").first().should("contain", "HTML"); // première compétence
+	  cy.get("#skills .container .card.skillsText").should("contain", "HTML"); // première compétence
 	});
   
 	it("should create portfolio cards from JSON data", () => {
 		// Vérifier que les cartes sont bien créées
 	  cy.get("#portfolio .container .card.portfolioContent").should("be.visible").and("have.length.greaterThan", 0);
-	  cy.get("#portfolio .container .card.portfolioContent").first().should("contain", "Kasa");
+	  cy.get("#portfolio .container .card.portfolioContent").should("contain", "Kasa");
 	});
   });
